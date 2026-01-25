535b29a4db3956af185c30a51b0bc86ae912de16
Yusuke Tanaka <wing0920@gmail.com>
2026-01-25 17:08:10 +0900

    open text/html in firefox

--- linux.nix
+++ linux.nix
@@ -7,6 +7,7 @@ lib.mkIf pkgs.stdenv.isLinux {
       "x-scheme-handler/http" = "firefox.desktop";
       "x-scheme-handler/https" = "firefox.desktop";
       "x-scheme-handler/about" = "firefox.desktop";
+      "text/html" = "firefox.desktop";
     };
   };
 
