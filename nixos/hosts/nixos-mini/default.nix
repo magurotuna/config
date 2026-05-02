8e315ebbe355c1243f2105a0c448f60f8c4b0316
Yusuke Tanaka <wing0920@gmail.com>
2026-05-02 11:27:41 +0900

    enable mosh

--- nixos/hosts/nixos-mini/default.nix
+++ nixos/hosts/nixos-mini/default.nix
@@ -9,6 +9,10 @@
 
   networking.hostName = "nixos-mini";
 
+  # Mosh: roaming, intermittent-connectivity-friendly remote shell.
+  # Enabling this installs the mosh package and opens UDP 60000-61000.
+  programs.mosh.enable = true;
+
   # https://nixos.org/nixos/options.html
   system.stateVersion = "25.11";
 }
